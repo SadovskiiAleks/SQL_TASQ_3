5227a9757561c810f800b8fc27337d8d2a82a162
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-09-18 22:07:50 +0300

    add seventh solution

--- src/main/resources/db.changelog/migration/schema_1.sql
+++ src/main/resources/db.changelog/migration/schema_1.sql
@@ -1,8 +1,9 @@
-create table hw_netology.CUSTUMERS (
+CREATE SCHEMA IF NOT EXISTS public;
+create table public.CUSTUMERS (
                            id SERIAL PRIMARY KEY ,
                            name varchar(50) not Null,
                            surname varchar(100) not Null,
                            age int check (age > 0 and age < 150),
                            phone_number varchar(11) not Null
 );
---rollback drop table hw_netology.CUSTUMERS;
+-- rollback drop table hw_netology.CUSTUMERS;
